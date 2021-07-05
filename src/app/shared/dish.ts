a89a5840b2e87e09e4f49a836adf8c3900f3db0a
disorder75 <nnzcastelli@gmail.com>
2021-07-05 18:28:08 +0200

    - data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comments } from './comments';
+
 export class Dish {
     id!: string;
     name!: string;
@@ -7,4 +9,6 @@ export class Dish {
     label!: string;
     price!: string;
     description!: string;
+
+    comments!: Comments[];
 }
